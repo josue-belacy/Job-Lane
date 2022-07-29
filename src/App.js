065b8f0f184cf1d280f0564caeed4ef91a365fc7
Josue Belacy <jbelacy@gmail.com>
2022-07-29 06:41:29 -0400

    Landing Page -Structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Landing from "./pages/Landing"
 function App() {
   return (
     <div>
-      
       <h1>JobLane</h1>
       <Landing />
     </div>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,25 @@
-import logo from '../assets/images/logo.svg'
+import logo from '../assets/images/logo.jpg'
 import main from '../assets/images/main.svg'
 const Landing = () => {
-  return <h4>Landing Page</h4>
+  return (
+  <main>
+    <nav>
+      <img src={logo} alt='joblane' className='logo' />
+    </nav>
+    <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span> tracking </span> App
+          </h1>
+          <p>
+            Take control of your job search with JobLane. JobLane will help you organize your prospects, so that you can pass interviews. With JobLane you'll be ready to negotiate offers in no time.
+          </p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main> 
+  )
 }
 
 export default Landing
