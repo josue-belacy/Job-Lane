9e5efea8445c34863cecb057ce375c7a32949f38
Josue Belacy <jbelacy@gmail.com>
2022-08-01 18:45:38 -0400

    Fix landing.js to address the import error on Ln 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import Landing from './pages/Landing'
 import styled from 'styled-components'
 
+const Button = styled.section.button`
+background: red;
+color: white;
+font-size: 1rem;
+
+`
+
 function App() {
   return (
     <div>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,25 +1,67 @@
-import logo from '../assets/images/logo.jpg'
+import main from '../assets/images/logo.svg'
 import main from '../assets/images/main.svg'
+import styled from 'styled-components'
 const Landing = () => {
   return (
-  <main>
-    <nav>
-      <img src={logo} alt='joblane' className='logo' />
-    </nav>
-    <div className="container page">
-        <div className="info">
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
           <h1>
-            Job <span> tracking </span> App
+            job <span>tracking</span> app
           </h1>
           <p>
-            Take control of your job search with JobLane. JobLane will help you organize your prospects, so that you can pass interviews. With JobLane you'll be ready to negotiate offers in no time.
+          Take control of your job search with JobLane. JobLane will help you organize your prospects, so that you can pass interviews. With JobLane you'll be ready to negotiate offers in no time.
           </p>
-          <button className='btn btn-hero'>Login/Register</button>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
         </div>
-        <img src={main} alt="job hunt" className="img main-img" />
+        <img src={main} alt='job hunt' className='img main-img' />
       </div>
-    </main> 
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    width: var(--fluid-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+}
+.page {
+  min-height: calc(100vh - var(--var-height));
+  display: grid;
+  align-items: center;
+  margin-top: -3rem;
+}
+h1 {
+  font-weight: 700;
+  span {
+    color: var(--primary-500);
+  }
+}
+p {
+  color: var(--grey-600);
+}
+.main-img {
+  display: none;
+}
+@media (min-width: 992px) {
+  .page {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 3rem;
+  }
+  .main-img {
+    display: block;
+  }
+}
+`
 
 export default Landing
